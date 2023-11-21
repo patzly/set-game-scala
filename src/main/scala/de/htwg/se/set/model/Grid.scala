176963524d9dec77037053b91d60682e334aeb24
Patrick Zedler <mail@patrickzedler.com>
2023-11-21 15:45:53 +0100

    Revert Grid factory method pattern implementation and further simplify Grid

--- src/main/scala/de/htwg/se/set/model/Grid.scala
+++ src/main/scala/de/htwg/se/set/model/Grid.scala
@@ -1,11 +1,11 @@
 package de.htwg.se.set.model
 
-sealed trait Grid(columns: Int, cards: List[Card]):
+case class Grid(columns: Int, cards: List[Card], easy: Boolean):
 
   if columns * 3 != cards.length then
     throw new IllegalArgumentException("Amount of cards has to be equal to the grid size")
-  
-  def columnWidth: Int
+
+  private def columnWidth: Int = if easy then 3 else 4
 
   private def legend(columns: Int): String =
     "  " + (65 until 65 + columns).map(_.toChar).map("│" + _ + (" " * (columnWidth - 1))).mkString
@@ -19,23 +19,6 @@ sealed trait Grid(columns: Int, cards: List[Card]):
       result.append((rowIndex + 1) + " ")
       for colIndex <- 0 until columns do
         val cardIndex = rowIndex + 3 * colIndex
-        val cardStr = if cardIndex < cards.length then
-          cards(cardIndex).toString
-        else
-          " " * columnWidth
-        result.append("│" + cardStr)
+        result.append("│" + (if cardIndex < cards.length then cards(cardIndex).toString else " " * columnWidth))
       result.append("\n")
     result.toString
-
-object Grid:
-
-  private class NormalGrid(columns: Int, cards: List[Card]) extends Grid(columns, cards):
-
-    override def columnWidth: Int = 4
-
-  private class EasyGrid(columns: Int, cards: List[Card]) extends Grid(columns, cards):
-
-    override def columnWidth: Int = 3
-
-  def apply(columns: Int, cards: List[Card], easy: Boolean): Grid =
-    if easy then EasyGrid(columns, cards) else NormalGrid(columns, cards)
