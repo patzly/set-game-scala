f545aa44aab7f10403724f4f259a6dc2e3705a8d
matthi <matthi.elbel@gmail.com>
2023-12-15 03:35:14 +0100

    Adding Component-Structure

---
+++ src/main/scala/de/htwg/se/set/manager/undoManagerInterface.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.set.manager
+
+trait undoManagerInterface {
+//TODO verknüpfung zum UndoManager (nicht sicher ob wirs hier brauchen)
+}
---
+++ src/main/scala/de/htwg/se/set/modelComponent/GameInterface.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.set.modelComponent
+
+trait GameInterface {
+//TODO Verknüpfung zu gameComponent.Game
+// Man könnte Action, Card, Command, Settings und State auch als einzelne Compnenten machen (einfach die Dateien
+// jeweils in einen unterpackage Packen und das in ihnen enthaltene Interface im ModelComponent package belassen
+// Ach ja und bei Triplet bin ich mir unsicher, ob wir es nicht auch als einzelnes Component brauchen, da wir auch von
+// Command drauf zugreifen (wenn wir des aber so machen, würden wir mit Deck von unten nach oben auf Triplet zugreifen,
+// was wir ja auch nichr sollen), ich würds einfach so lassen
+}
